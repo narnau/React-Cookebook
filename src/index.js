0e760d6f8868649953033843cc3868e759519eaa
Arnau Julià <arnau.julia@gmail.com>
2020-04-29 18:09:15 +0200

    added redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ReduxProvider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ReduxProvider>,
+
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
