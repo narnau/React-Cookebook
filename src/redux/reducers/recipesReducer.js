f2716d74a43395015400d83ebcbca927a2fba15a
Arnau Julià <arnau.julia@gmail.com>
2020-05-06 10:19:43 +0200

    improvements in the recipesReducer

--- src/redux/reducers/recipesReducer.js
+++ src/redux/reducers/recipesReducer.js
@@ -5,31 +5,14 @@ export default function recipesReducer(state = initialState, action) {
   let newState;
   switch (action.type) {
     case types.CREATE_RECIPE_SUCCESS:
-      newState = Object.assign(
-        {},
-        {
-          filter: state.filter,
-          list: [...state.list, { ...action.recipe }],
-        }
-      );
-      return newState;
-    case types.EDIT_RECIPE_SUCCESS:
-      newState = Object.assign(
-        {},
-        {
-          filter: state.filter,
-          list: [...state.list, { ...action.recipe }],
-        }
-      );
+      newState = Object.assign({}, state, {
+        list: [...state.list, { ...action.recipe }],
+      });
       return newState;
     case types.LOAD_RECIPES_SUCCESS:
-      newState = Object.assign(
-        {},
-        {
-          filter: state.filter,
-          list: action.recipes,
-        }
-      );
+      newState = Object.assign({}, state, {
+        list: action.recipes,
+      });
       return newState;
 
     case types.FILTER_RECIPES:
